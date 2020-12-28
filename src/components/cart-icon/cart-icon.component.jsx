9350d08fc14bdf180cc704e5ada863de97323bea
Saha <chiranjit.saha@optum.com>
2020-12-28 20:59:08 +0530

    add reselect package and implemented cart selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 import { toggleCartHidden } from "../../redux/cart/cart.action";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, items }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{items}</span>
   </div>
 );
 
@@ -17,7 +18,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+const mapStateToProps = state => ({
+  items: selectCartItemsCount(state)
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
